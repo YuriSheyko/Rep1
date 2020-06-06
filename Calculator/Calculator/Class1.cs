7845a26197f11b9b9bb7a9ce8af7d34db3e7ef6a
Iurii Sheiko <sheyko@sfedu.ru>
2020-06-06 20:13:50 +0300

    commit1

---
+++ Calculator/Calculator/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Class1
+    {
+        public static double Calculate(double value1, double value2, string mathOperator)
+        {
+            double result = 0;
+
+            switch (mathOperator)
+            {
+                case "/":
+                    result = value1 / value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+            }
+
+            return result;
+
+        }
+    }
+}
